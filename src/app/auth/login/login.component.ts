6ab129ae3209d1c72e6f6c7ec17d46f5536883a7
FrancescoNaccari <francesco.n92@gmail.com>
2024-04-09 14:41:16 +0200

    login + grafic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  @ViewChild('container') container!: ElementRef;
+
+  signIn() {
+    this.container.nativeElement.classList.remove('right-panel-active');
+  }
+
+  signUp() {
+    this.container.nativeElement.classList.add('right-panel-active');
+  }
 }
+
